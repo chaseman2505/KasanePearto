a04e0dd3a0bb8c106ff4e9f4067a8ed1b78e775a
chaseman2505 <chaseman2505@gmail.com>
2025-10-27 15:01:54 -0400

    Updating branch with all changes

---
+++ ghost-wranglers/DoorObject.cs
@@ -0,0 +1,27 @@
+using Godot;
+using System;
+
+public partial class DoorObject : WorldObject
+{
+	//If the door is open
+	bool isOpen = false;
+
+	public override void TriggerInteraction()
+	{
+		GD.Print(turnManager.ActiveCharacters[turnManager.CurrentCharacterIndex] + " is interacting with" + this.Name);
+		turnManager.LabelUI.Text = turnManager.ActiveCharacters[turnManager.CurrentCharacterIndex].Name + " is interacting with " + this.Name;
+
+		//Changes the state of the doorObject
+		isOpen = !isOpen;
+
+		//Changes the texture of the sprite (the sprite2D must be the first child of the door object)
+		if (isOpen)
+		{
+			GetChild<Sprite2D>(0).Texture = GD.Load<Texture2D>("res://Obstacle.png");
+		}
+		else
+		{
+			GetChild<Sprite2D>(0).Texture = GD.Load<Texture2D>("res://icon.svg");
+		}
+	}
+}
